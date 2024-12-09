5592d4decdf34624c10e5cff836fcd8fe9ae86a9
Aedromer <Aedromer@users.noreply.github.com>
2024-12-09 15:29:50 +0100

    limited camera gizmos (not quite working correctly)

--- Assets/Scripts/LimitedCamera.cs
+++ Assets/Scripts/LimitedCamera.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,11 +15,11 @@ public class LimitedCamera : MonoBehaviour
     float referenceVerticalRotation;
 
     // relative to reference rotation
-    [Range(-180, 0)] public float minHorizontalRotation;
-    [Range(0, 190)] public float maxHorizontalRotation;
-    [Range(-90, 0)] public float minVerticalRotation;
-    [Range(0, 90)] public float maxVerticalRotation;
-
+    [Range(-0.1f, -90f)] public float minHorizontalRotation;
+    [Range(0.1f, 90f)] public float maxHorizontalRotation;
+    [Range(-0.1f, -90f)] public float minVerticalRotation;
+    [Range(0.1f, 90f)] public float maxVerticalRotation;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -48,26 +50,31 @@ public class LimitedCamera : MonoBehaviour
         transform.localEulerAngles = Vector3.right * cameraVerticalRotation + Vector3.up * cameraHorizontalRotation;
     }
 
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
-        //absolute values
-        float minHRot = referenceHorizontalRotation + minHorizontalRotation;
-        float maxHRot = referenceHorizontalRotation + maxHorizontalRotation;
-        float minVRot = referenceVerticalRotation + minVerticalRotation;
-        float maxVRot = referenceVerticalRotation + maxVerticalRotation;
-
-        //Vector3 cornerDirLU = Quaternion.Euler(minVRot, minHRot, 0) * Vector3.right;
-        //Gizmos.DrawRay(transform.position, cornerDirLU, Color.red, float.PositiveInfinity);
+        // absolute values
+        float minHRot = transform.localEulerAngles.y + minHorizontalRotation;
+        float maxHRot = transform.localEulerAngles.y + maxHorizontalRotation;
+        float minVRot = transform.localEulerAngles.x + minVerticalRotation;
+        float maxVRot = transform.localEulerAngles.x + maxVerticalRotation;
 
         float fovVert = maxVRot - minVRot;
         float fovHori = maxHRot - minHRot;
-        float aspect  = fovHori / fovVert;
 
-        float centralDirV = maxVRot - minVRot / 2;
-        float centralDirH = maxHRot - minHRot / 2;
+        // add the camera's fov, so we can see the actual edges of what can be seen
+        Camera cam = GetComponent<Camera>();
+        fovVert += cam.fieldOfView;
+        fovHori += cam.fieldOfView * cam.aspect;
+
+        float aspect = fovHori / fovVert;
+
+        float centralDirV = (maxVRot + minVRot) / 2;
+        float centralDirH = (maxHRot + minHRot) / 2;
         float dirDiffV = centralDirV - referenceVerticalRotation;
         float dirDiffH = centralDirH - referenceHorizontalRotation;
 
-        Gizmos.DrawFrustum(transform.position, fovVert, 10, 1, aspect);
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.Euler(centralDirV, centralDirH, 0), transform.localScale);
+        Gizmos.color = Color.red;
+        Gizmos.DrawFrustum(Vector3.zero, fovVert, 100, 1, aspect);
     }
 }
