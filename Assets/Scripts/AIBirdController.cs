e8fd6445df1c6d2d91be047895085668b7d8fbe9
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2024-12-12 18:03:15 +0100

    added fake actions for test prefabs

--- Assets/Scripts/AIBirdController.cs
+++ Assets/Scripts/AIBirdController.cs
@@ -10,6 +10,14 @@ public class AIBirdController : MonoBehaviour
     private float actionTime; //waiting time between start of random actions
     public delegate void RandomActions();
 
+    // for testing
+    public float maxSize;
+    public float growFactor;
+    public float waitTime;
+
+    public float y;
+    public float speed;
+
     private void Start()
     {
         actionTime = 1;
@@ -47,15 +55,79 @@ public class AIBirdController : MonoBehaviour
     public void PerformChirp()
     {
         Debug.Log("Chirp! Chirp!");
+        StartCoroutine(Scale());
+
     }
 
     public void PerformShit()
     {
         Debug.Log("Poops ahoy!");
+        TestJump();
     }
 
     public void PerformEat()
     {
         Debug.Log("I am eating!");
+        StartCoroutine(Spin());
+    }
+
+    // test actions
+    IEnumerator Scale()
+    {
+        float timer = 0;
+
+        while (true) // this could also be a condition indicating "alive or dead"
+        {
+            // we scale all axis, so they will have the same value, 
+            // so we can work with a float instead of comparing vectors
+            while (maxSize > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+            // reset the timer
+
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (1 < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    void TestJump()
+    {
+        if (y >= 3)
+        {
+            y = y - speed * Time.deltaTime;
+        }
+        if (y < 3)
+        {
+            y = y + speed * Time.deltaTime;
+        }
+        transform.position = new Vector3(0, y, 0);
+    }
+
+    IEnumerator Spin()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2.0f);
+            float timer = 0f;
+            while (timer < 1)
+            {
+                transform.Rotate(0, 0, 180 * Time.deltaTime);
+                timer = timer + Time.deltaTime;
+                yield return null;
+            }
+        }
     }
 }
--- Assets/Scripts/AIRobotController.cs
+++ Assets/Scripts/AIRobotController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AIRobotController : AIBirdController
 {
+    public float minSize;
+
     public override void PerformRandomAction()
     {
         List<RandomActions> randomActions = new List<RandomActions>
         {
-            PerformChirp,   
-            PerformShit,   
+            PerformChirp,
+            PerformShit,
             PerformEat,     
             PerformSpyAct,  
             ShowAntennae    
@@ -21,10 +23,66 @@ public class AIRobotController : AIBirdController
     public void PerformSpyAct()
     {
         Debug.Log("I am watching u!");
+        StartCoroutine(MakeSmall());
     }
 
     public void ShowAntennae()
     {
         Debug.Log("Beep beep!");
+        StartCoroutine(ChangeColor());
     }
+
+    //test actions
+    IEnumerator ChangeColor()
+    {
+        var cubeRenderer = GetComponent<Renderer>();
+
+        while (true)
+        {
+            cubeRenderer.material.SetColor("_Color", Color.blue);
+            yield return new WaitForSeconds(1.0f);
+            float timer = 0f;
+            while (timer < 2)
+            {
+                cubeRenderer.material.SetColor("_Color", Color.red);
+                timer = timer + Time.deltaTime;
+                yield return null;
+            }
+
+            cubeRenderer.material.SetColor("_Color", Color.blue);
+        }
+        
+    }
+
+    IEnumerator MakeSmall()
+    {
+        float timer = 0;
+
+        while (true) // this could also be a condition indicating "alive or dead"
+        {
+            // we scale all axis, so they will have the same value, 
+            // so we can work with a float instead of comparing vectors
+            while (minSize < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(0.3f, 0.3f, 0.3f) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+            // reset the timer
+
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (1 > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(0.3f, 0.3f, 0.3f) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
 }
