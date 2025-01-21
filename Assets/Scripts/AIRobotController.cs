23773ddc5c5eb141881fad9fbfe2538a4057cf57
Maria Smirnova <92306135+MarieMittu@users.noreply.github.com>
2025-01-21 15:36:54 +0100

    rectangle waklking area for robots

--- Assets/Scripts/AIRobotController.cs
+++ Assets/Scripts/AIRobotController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIRobotController : AIBirdController
 {
-    public bool isSpying = false;
+    [HideInInspector] public bool isSpying = false;
+
+    public Vector3 rectangleSize = new Vector3(20, 0, 10);
 
     private void Update()
     {
@@ -33,4 +36,38 @@ public class AIRobotController : AIBirdController
         Sleep("R02_Sitting_Sleeping", "02_Sitting_Standing_up");
         isSpying = true;
     }
+
+    protected override bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        float halfX = rectangleSize.x / 2;
+        float halfZ = rectangleSize.z / 2;
+
+        float randomX = Random.Range(-halfX, halfX);
+        float randomZ = Random.Range(-halfZ, halfZ);
+        Vector3 randomPoint = new Vector3(center.x + randomX, center.y, center.z + randomZ);
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 10.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(centrePoint, rectangleSize);
+
+         if (agent != null && agent.hasPath)
+        {
+            Gizmos.color = Color.magenta; // Destination point color
+            Gizmos.DrawSphere(agent.destination, 0.5f); // Visualize the agent's current destination
+        }
+    }
+
 }
